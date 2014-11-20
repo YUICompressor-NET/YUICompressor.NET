0591cd60dac62a341e5450e72967922957bdd8ea
James <code-for-coffee@users.noreply.github.com>
2014-11-20 08:54:06 -0600

    Updating AssemblyCompany per request
    Requested by @PureKrome / @freeranger

--- Code/Shared Assembly Info/SharedAssemblyInfo.cs
+++ Code/Shared Assembly Info/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyCompany("PureKrome & Yahoo")]
+[assembly: AssemblyCompany("The PureRanger Partnership")]
 [assembly: AssemblyProduct("YUI Compressor .NET")]
 [assembly: AssemblyCopyright("Copyright © 2008")]
 [assembly: AssemblyTrademark("")]
